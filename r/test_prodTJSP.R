daa926e95da43c6c96f86bad877b047c81b87108
Caio Moreno <caiomsouza@gmail.com>
2017-06-26 11:24:28 +0200

    Test

---
+++ r/test_prodTJSP.R
@@ -0,0 +1,23 @@
+
+
+
+# https://github.com/abjur/prodTJSP
+
+
+
+if (!require(devtools)) install.packages('devtools')
+devtools::install_github('abjur/prodTJSP')
+
+library("prodTJSP")
+
+d_list <- prod_download_courts()
+
+d_list <- prod_list()
+
+prod_download(d_list, path = 'data-raw/pdf')
+
+# suppose your pdf files are in data-raw/pdf folder
+arqs <- dir('data-raw/pdf', full.names = TRUE)
+d_prod <- prod_parse_uni(arqs)
+d_prod
+
